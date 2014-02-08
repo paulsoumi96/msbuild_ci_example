98f8f06ad36ac5e18f69cd4d2f66b0b1add8e195
Manoj <manojlds@gmail.com>
2014-02-09 03:31:37 +0530

    Adding project files

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace msbuild_ci_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MSBuild CI with TeamCity!");
+        }
+    }
+}
