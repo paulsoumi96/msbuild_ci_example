63f25d3c355d6096dea5227430c1155733d83b3c
Manoj <manojlds@gmail.com>
2014-02-09 23:49:52 +0530

    Adding nunit tests

---
+++ tests/ProgramTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace msbuild_ci_example.tests
+{
+    [TestFixture]
+    class ProgramTests
+    {
+        [Test]
+        public void ShouldBeADummyTest()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
